4427fa45010f12621bb0eecf0d3140fb369ff8dd
Mario-PC <mags9415@gmail.com>
2024-03-13 16:36:36 -0600

    se termino el maquetado del componente Experience y procedemos con el componente de contacto.

--- src/views/Experiences.tsx
+++ src/views/Experiences.tsx
@@ -2,10 +2,83 @@ import "../styles/experiences.scss";
 const Experiences = () => {
   return (
     <>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus ipsam
-      est, accusantium ab asperiores suscipit dicta explicabo, aliquid similique
-      laudantium, obcaecati sapiente voluptas enim impedit modi facilis.
-      Corrupti, officiis voluptas!
+      <section className="containerExperience">
+        <div className="textExperience">
+          <h2>Experiencia adquirida</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est iusto
+            voluptatem iste perspiciatis ipsum saepe consequuntur odit ea neque
+            architecto consequatur excepturi vitae enim facere natus, maiores
+            ipsa? Impedit, maxime!
+          </p>
+        </div>
+        <div className="content-experience">
+          <div className="cardContainer">
+            <div className="textCard">
+              <p className="tituloCard">Titulo del proyecto</p>
+              <p className="descriptionCard">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Molestias ducimus tempora tempore soluta! Molestias libero
+                doloribus quidem in pariatur et nihil numquam eum eius dolorum
+                ducimus, facere ullam rem dignissimos?
+              </p>
+              <div className="contentBtn">
+                <button className="enlaceBtn">
+                  <span>Enlace</span>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="cardContainer">
+            <div className="textCard">
+              <p className="tituloCard">Titulo del proyecto</p>
+              <p className="descriptionCard">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Molestias ducimus tempora tempore soluta! Molestias libero
+                doloribus quidem in pariatur et nihil numquam eum eius dolorum
+                ducimus, facere ullam rem dignissimos?
+              </p>
+              <div className="contentBtn">
+                <button className="enlaceBtn">
+                  <span>Enlace</span>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="cardContainer">
+            <div className="textCard">
+              <p className="tituloCard">Titulo del proyecto</p>
+              <p className="descriptionCard">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Molestias ducimus tempora tempore soluta! Molestias libero
+                doloribus quidem in pariatur et nihil numquam eum eius dolorum
+                ducimus, facere ullam rem dignissimos?
+              </p>
+              <div className="contentBtn">
+                <button className="enlaceBtn">
+                  <span>Enlace</span>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="cardContainer">
+            <div className="textCard">
+              <p className="tituloCard">Titulo del proyecto</p>
+              <p className="descriptionCard">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Molestias ducimus tempora tempore soluta! Molestias libero
+                doloribus quidem in pariatur et nihil numquam eum eius dolorum
+                ducimus, facere ullam rem dignissimos?
+              </p>
+              <div className="contentBtn">
+                <button className="enlaceBtn">
+                  <span>Enlace</span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
