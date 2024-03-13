3252d77736d5b67f0a8418b00a783a901efde98a
Mario-PC <mags9415@gmail.com>
2024-03-13 03:01:57 -0600

    salio un issue de el footer y ya lo reparamos. Continuamos con el componente experiences

--- src/views/Experiences.tsx
+++ src/views/Experiences.tsx
@@ -1,4 +1,12 @@
+import "../styles/experiences.scss";
 const Experiences = () => {
-  return <div>Experiences</div>;
+  return (
+    <>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus ipsam
+      est, accusantium ab asperiores suscipit dicta explicabo, aliquid similique
+      laudantium, obcaecati sapiente voluptas enim impedit modi facilis.
+      Corrupti, officiis voluptas!
+    </>
+  );
 };
 export default Experiences;
