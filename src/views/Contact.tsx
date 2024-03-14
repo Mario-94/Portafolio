216a519159782bbc3757744a403ce42bd5cebfbe
Mario-PC <mags9415@gmail.com>
2024-03-13 22:55:40 -0600

    terminamos la parte de contactos y damos por finalizada la parte de portafolio.

--- src/views/Contact.tsx
+++ src/views/Contact.tsx
@@ -1,4 +1,29 @@
+import "../styles/contact.scss";
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <>
+      <section className="containerContact">
+        <div className="content-contact">
+          <h2 className="textContacto">Contacto</h2>
+          <p className="descriptionContact">
+            ¿Necesitas ayuda con el desarrollo de tu proyecto web? ¡Estoy aquí
+            para ayudarte en todo lo que necesites, ya sea en el front-end o en
+            el back-end!
+          </p>
+          <p className="descriptionContact">
+            E-mail:&nbsp;
+            <a
+              href="mailto:mags9415@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+              className="link"
+            >
+              ↗ mags9415@gmail.com
+            </a>
+          </p>
+        </div>
+      </section>
+    </>
+  );
 };
 export default Contact;
