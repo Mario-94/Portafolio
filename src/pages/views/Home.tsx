8a1940cb45e4323a549c682156c9c26d8059d23d
Mario-PC <mags9415@gmail.com>
2025-08-12 00:37:09 -0600

    refactor: Acomode la parte de los estilos de tailwind, porque no los tomaba, se arreglo el primer mensaje para mi portafolio, que es el mensaje acerca de mi

--- src/pages/views/Home.tsx
+++ src/pages/views/Home.tsx
@@ -12,33 +12,52 @@ import {
 const Home = () => {
   return (
     <>
-      <section className="container-home">
-        <div className="container-description-person border-2">
-          <h1>Mario-94</h1>
-          <div className="description-person">
-            <p>Mario Alberto Garcia Sanchez</p>
-            <span>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat
-              eos labore iste, voluptates enim impedit aut voluptatibus rerum?
-              Quae dolor fugiat delectus deleniti. Voluptas, facere corporis
-              vero esse laudantium assumenda.
-            </span>
-            <button className="button-container">
-              <span>Conoceme</span>
-            </button>
+      <section className="w-full bg-background h-full text-paragraph mt-4 md:px4 lg:px-8">
+        <section className="w-full h-[24rem] max-w-9xl  mx-auto  grid md:grid-cols-2 grid-rows-1 gap-2">
+          {/* Texto */}
+          <div className="flex flex-col justify-center gap-4 px-4 border-2">
+            <header>
+              <h1 className="pb-1">Mario-94</h1>
+              <h2 className="">Mario Alberto Garcia Sanchez</h2>
+            </header>
+            <p className="text-paragraph line-clamp-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+              similique eius eligendi ad. Quaerat, ut autem dolore at non quis
+              et. Natus deserunt amet provident, ipsum similique voluptatibus
+              quos soluta! Doloribus eaque, aperiam in obcaecati ipsa qui ipsam
+              quibusdam sint neque illum tenetur. Ullam eaque commodi explicabo!
+              Quasi fugit necessitatibus dignissimos, libero, autem et in dolor,
+              illo asperiores cum placeat. Suscipit perferendis fuga totam
+              doloribus maiores rerum ducimus praesentium quae, eaque
+              repudiandae. Sint, possimus consequatur! Iure tempore ipsum quis
+              quasi nesciunt beatae aut laudantium a! Repellat accusantium
+              officiis quaerat nostrum. Excepturi ducimus soluta, pariatur vero
+              saepe ea veniam praesentium exercitationem fugit ipsum iusto
+              reprehenderit. Quisquam ullam beatae enim dolorum labore ipsum
+              voluptas, sunt temporibus fugiat, aspernatur repudiandae rem
+              tempora qui? Eos, quos sapiente a, assumenda accusantium libero et
+              placeat amet commodi architecto provident corrupti omnis labore
+              voluptate, iste vero qui porro quae numquam ipsa similique
+              veritatis id? Nemo, dicta neque.
+            </p>
+            <div className="w-3/4 md:w-full flex justify-center">
+              <button className="bg-button w-1/2 text-buttonText px-4 py-2 rounded-3xl transition transform hover:-translate-y-1 hover:shadow-md hover:bg-button/90">
+                Conóceme
+              </button>
+            </div>
           </div>
-        </div>
-        <div className="avatar-img">
-          <div className="content-avatar">
+
+          {/* Imagen */}
+          <div className="flex items-center justify-center md:justify-end px-4 border-2">
             <img
               src="https://img.freepik.com/foto-gratis/pintura-lago-montana-montana-al-fondo_188544-9126.jpg"
               alt="avatar-img"
-              className="style-avatar-img"
+              className="w-3/4 rounded-3xl object-contain"
             />
           </div>
-        </div>
+        </section>
 
-        <div className="tecnologi">
+        <div className="w-full h-[24rem] max-w-8xl mx-auto   grid grid-cols-2 gap-2">
           <span className="carruselTec">
             <IconHtml />
           </span>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,32 @@ export const content = [
     "./src/**/*.{js,ts,jsx,tsx,css}",
 ];
 export const theme = {
-    extend: {},
+    extend: {
+        colors: {
+            // elements
+            background: "#004643",
+            headline: "#fffffe",
+            paragraph: "#abd1c6",
+            button: "#f9bc60",
+            buttonText: "#001e1d",
+            // illustration
+            strongText: "#001e1d",
+            main: "#e8e4e6",
+            highLight: "#f9bc60",
+            secondary: "#abd1c6",
+            tertiary: "#e16162",
+        },
+        keyframes: {
+            scroll: {
+                "0%": { transform: "translateX(0)" },
+                "100%": { transform: "translateX(-50%)" },
+            },
+        },
+        animation: {
+            "scroll-fast": "scroll 10s linear infinite",
+            "scroll-medium": "scroll 20s linear infinite",
+            "scroll-slow": "scroll 30s linear infinite",
+        },
+    },
 };
 export const plugins = [];
